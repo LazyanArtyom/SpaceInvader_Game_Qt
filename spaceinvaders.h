4ea822a6e2db379d05771c6fc352a86d2315dcb6
LazyanArtyom <artyomlazyan@gmail.com>
2020-02-20 15:20:19 +0400

    Space Invaders Final version

---
+++ spaceinvaders.h
@@ -0,0 +1,35 @@
+#ifndef SPACEINVADERS_H
+#define SPACEINVADERS_H
+
+#include "components.h"
+
+#include <QPointer>
+#include <QGraphicsView>
+#include <QKeyEvent>
+
+class CSpaceInvaders : public QGraphicsView
+{
+    Q_OBJECT
+public:
+    CSpaceInvaders(QSize oScreenSize, QWidget* pParent = nullptr);
+
+    void Run();
+    void CheckPoints();
+
+protected:
+    void keyPressEvent(QKeyEvent* pEvent) override;
+
+public slots:
+    void onCreateEnemy();
+    void onIncreaseScore();
+    void onDecreaseScore();
+    void onDecreaseHealth();
+    void onGameOver();
+
+private:
+    CCannon*    m_pCannon = nullptr;
+    CPoints*    m_pPoints = nullptr;
+    QSize       m_oScreenSize;
+};
+
+#endif // SPACEINVADERS_H
